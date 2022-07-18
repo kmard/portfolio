836a13a01494c86d6ee92e5ef367ff8dacec15b5
KM <tsnok777@gmail.com>
2022-07-18 19:13:38 +0300

    output query set home page

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from portfolio.models import Project
 
 
 # Create your views here.
 def home(request):
+    projects = Project.objects.all()
 
-    return render(request,'portfolio/home.html',{})
+    return render(request,'portfolio/home.html',{'projects':projects})
