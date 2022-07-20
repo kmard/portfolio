9f4a700bb47b7263105cc2b9dd00dc88a1cd6176
KM <tsnok777@gmail.com>
2022-07-20 14:25:51 +0300

    create blogs page and connect to site

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog_model(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField()
+
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import blog_model
 
 # Create your views here.
 def all_blogs(request):
-    return render(request,'blog/all_blogs.html')
+
+    blogs = blog_model.objects.all()
+
+    return render(request,'blog/all_blogs.html',{'blogs':blogs})
