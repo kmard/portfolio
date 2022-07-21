5d125d14b008698580158bf7919c61a620ceb0f3
KM <tsnok777@gmail.com>
2022-07-21 14:25:06 +0300

    Output detail of blog

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import blog_model
 
 # Create your views here.
@@ -8,3 +8,7 @@ def all_blogs(request):
     blogs = blog_model.objects.order_by('-date')
 
     return render(request,'blog/all_blogs.html',{'blogs':blogs})
+
+def detail(request,blog_id):
+    blog = get_object_or_404(blog_model,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':blog})
