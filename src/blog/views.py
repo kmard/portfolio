7f483597a1eb45508c7a42bbbca2d9b4478ff49e
KM <tsnok777@gmail.com>
2022-08-21 13:20:54 +0300

    upload user interface

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,8 +7,8 @@ def all_blogs(request):
     # blogs = blog_model.objects.order_by('-date')[:5]
     blogs = blog_model.objects.order_by('-date')
 
-    return render(request,'blog/all_blogs.html',{'blogs':blogs})
+    return render(request,'blog/all_blogs.html',{'blogs':blogs,'showBlog':False})
 
 def detail(request,blog_id):
     blog = get_object_or_404(blog_model,pk=blog_id)
-    return render(request,'blog/detail.html',{'blog':blog})
+    return render(request,'blog/detail.html',{'blog':blog,'showBlog':True})
