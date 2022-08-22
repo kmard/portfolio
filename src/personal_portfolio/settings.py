32650271cd9eee614ff8cc88ea15133f5c5e79ed
kmard <tsnok777@gmail.com>
2022-08-22 09:18:09 +0000

    Remove old staff

--- src/personal_portfolio/settings.py
+++ src/personal_portfolio/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=@faon8@hhv#8#7$q&m45j)i!&wutp6mc1dq84)-h-i7w4@z7e'
+SECRET_KEY = '=@faon8@hhv#8#7$q&m45j)i!&wutp6mc1dq84)-h-i7w4@z7e12Hqsd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kot78.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
