75a88dac998a5cf08fbef0954733b534ffafa171
KM <tsnok777@gmail.com>
2022-08-28 14:40:37 +0300

    upload user interface

--- src/personal_portfolio/settings.py
+++ src/personal_portfolio/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You are on production')
